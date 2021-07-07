9aebe664a5990a4d24069786fa5c3d1be22fe2b3
janakkapadia <kapadia.janak@gmail.com>
2021-07-07 15:54:40 +0530

    add auth user on creating

--- src/Traits/HasComments.php
+++ src/Traits/HasComments.php
@@ -6,6 +6,9 @@ use JanakKapadia\Commentable\Comment;
 trait HasComments
 {
     public static function bootCommentable() {
+        self::creating(function ($model){
+            $model->user = config('auth.providers.users.model');
+        });
         self::deleting(function ($model){
             $model->comments->each->delete();
         });

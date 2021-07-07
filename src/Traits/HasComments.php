e350a4ce18b33b4e98a63acbcdfb78953e144cfe
janakkapadia <kapadia.janak@gmail.com>
2021-07-07 17:00:09 +0530

    Auth user refactor

--- src/Traits/HasComments.php
+++ src/Traits/HasComments.php
@@ -7,12 +7,6 @@ use JanakKapadia\Commentable\Comment;
 trait HasComments
 {
     public static function bootCommentable() {
-        self::creating(function ($model) {
-            if (auth()->check()) {
-                $model->user_id = auth()->id();
-            }
-        });
-
         self::deleting(function ($model){
             $model->comments->each->delete();
         });

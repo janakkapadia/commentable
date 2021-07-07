cbeffb44c231dc6d9bde4d33887a7850e842fbef
janakkapadia <kapadia.janak@gmail.com>
2021-07-07 16:49:48 +0530

    Auth user on creating comment

--- src/Traits/HasComments.php
+++ src/Traits/HasComments.php
@@ -7,6 +7,12 @@ use JanakKapadia\Commentable\Comment;
 trait HasComments
 {
     public static function bootCommentable() {
+        self::creating(function ($model) {
+            if (auth()->check()) {
+                $model->user_id = auth()->id();
+            }
+        });
+
         self::deleting(function ($model){
             $model->comments->each->delete();
         });
